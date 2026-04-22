7a8495969f0d1c173aee31933a1d491f01cbec1e
amsyari-xv <amsyarifaris@gmail.com>
2026-04-22 20:47:19 +0800

    Update T2Q2.cpp

--- T2Q2.cpp
+++ T2Q2.cpp
@@ -1,24 +1,52 @@
 #include <iostream>
 using namespace std;
 
-void drawBox(int w);
+void drawBox(int w){
+
+    for (int h = 0; h < w; h++){
+        
+            cout << "# ";
+        }
+
+        cout << endl;
+    }
+
+
+void drawEmptyLine(int w){
+
+    for (int i = 0; i < w - 2; i++) {
+
+        cout << "# "; 
+
+        for (int j = 0; j < w - 2; j++) {
+
+            cout << "  ";
+        }
+        cout << "#" << endl;
+    }
+}
+
+void drawFullLine(int w) {
+    if (w < 2) return;
+    
+    drawBox(w);
+    drawEmptyLine(w);
+    drawBox(w);
+}
+
+
 
 int main(){
 
     int width;;
     cout << "\n Enter the side of the square box 1 - 20: ";
     cin >> width;
-    drawBox(width);
     
-} 
 
-void drawBox(int w){
-
-    for (int h = 0; h<w; ++h){
-        for(int l = 0; l<w; ++l){
-            cout << "# ";
-        }
-
-        cout << endl;
+    if (width >= 2) {
+        drawFullLine(width);
+    } else {
+        cout << "Side length must be at least 2." << endl;
     }
-}
+    return 0;
+} 
